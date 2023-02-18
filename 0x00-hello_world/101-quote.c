73e328bbae898700db4be6444e37843e00739237
Kingsjona <chukwudikingsley675@gmail.com>
2023-02-18 09:55:05 -0800

    printing without printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <un$std.h>
+/**
+ * main - printing a c program to standard error
+ * Return: 1
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
